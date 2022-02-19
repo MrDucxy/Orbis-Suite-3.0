b2224cee87c49a841a8746409c0c957f6c39b759
Greg <Greg@hackingadventures.ca>
2022-02-19 09:48:06 -0700

    Add project files.

---
+++ Windows/OrbisSuiteService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbisSuiteService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            OrbisSuiteService Service = new OrbisSuiteService();
+
+            Service.DebugStart();
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new OrbisSuiteService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
