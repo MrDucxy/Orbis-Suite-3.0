77bec51ff12d2ab8b10b1905d99ec76cf0db4bb1
Greg <Greg@hackingadventures.ca>
2022-02-19 23:13:47 -0700

    Implementing Neighborhood design. + Adding Assets

---
+++ Windows/OrbisNeighborHood/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using OrbisNeighborHood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbisNeighborHood.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        // Commands
+        public RelayCommand DashboardViewCommand { get; set; }
+
+        public RelayCommand TargetViewCommand { get; set; }
+
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        // View Models
+        public DashboardViewModel DashboardHomeVM { get; set; }
+
+        public TargetViewModel TargetVM { get; set; }
+
+        public SettingsViewModel SettingsVM { get; set; }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            DashboardHomeVM = new DashboardViewModel();
+            TargetVM = new TargetViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = DashboardHomeVM;
+
+            DashboardViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DashboardHomeVM;
+            });
+
+            TargetViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TargetVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
