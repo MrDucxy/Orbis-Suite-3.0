375f4a49ed4bd639dc1b3292778c3cc435b54a5f
Greg <Greg@hackingadventures.ca>
2022-04-25 17:32:03 -0600

    Installer UI now works :D Unfortunately .NET FW 4.8 and I had to make a second SimpleUI for .NET FW :laughing:

---
+++ Windows/Installer/SetupBA/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SimpleUI.Controls;
+using System.Windows;
+
+namespace SetupBA
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : SimpleWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
